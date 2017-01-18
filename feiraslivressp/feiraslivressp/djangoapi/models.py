f36b9a9b769abb0ce94aded53a8ac816d33b39ac
Rocky Shimithy <shimithy@gmail.com>
2017-01-18 14:44:40 -0200

    Model Feiras, but djangoapi app doesn't work

--- feiraslivressp/feiraslivressp/djangoapi/models.py
+++ feiraslivressp/feiraslivressp/djangoapi/models.py
@@ -1,57 +1,29 @@
 from django.db import models
 
-#class Feiras(models.Model):
 
-
-# class FeirasLivresSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     LONG = serializers.CharField(requerid=True, allow_blank=True,
-#                                  max_length=20)
-#     LAT = serializers.CharField(requerid=True, allow_blank=True,
-#                                 max_length=20)
-#     SETCENS = serializers.IntegerField(requerid=True, allow_blank=True,
-#                                        max_length=20)
-#     AREAP = serializers.IntegerField(requerid=True, allow_blank=True,
-#                                      max_length=20)
-#     CODDIST = serializers.IntegerField(requerid=True, allow_blank=True,
-#                                        max_length=5)
-#     DISTRITO = serializers.CharField(requerid=True, allow_blank=True,
-#                                      max_length=20)
-#     CODSUBPREF = serializers.IntegerField(requerid=True, allow_blank=True,
-#                                           max_length=5)
-#     SUBPREFE = serializers.CharField(requerid=True, allow_blank=True,
-#                                      max_length=50)
-#     REGIAO5 = serializers.CharField(requerid=True, allow_blank=True,
-#                                     max_length=20)
-#     REGIAO8 = serializers.CharField(requerid=True, allow_blank=True,
-#                                     max_length=20)
-#     NOME_FEIRA = serializers.CharField(requerid=True, allow_blank=True,
-#                                        max_length=50)
-#     REGISTRO = serializers.CharField(requerid=True, allow_blank=True,
-#                                      max_length=20)
-#     LOGRADOURO = serializers.CharField(requerid=True, allow_blank=True,
-#                                        max_length=50)
-#     NUMERO = serializers.IntegerField(requerid=True, allow_blank=True,
-#                                       max_length=6)
-#     BAIRRO = serializers.CharField(requerid=True, allow_blank=True,
-#                                    max_length=50)
-#     REFERENCIA = serializers.CharField(requerid=True, allow_blank=True,
-#                                        max_length=50)
-
-    # def create(self, validated_data):
-    #     """
-    #     Create and return a new `FeiraLivre`, given the validated data.
-    #     """
-    #     return Snippet.objects.create(**validated_data)
-
-    # def update(self, instance, validated_data):
-    #     """
-    #     Update and return an existing `FeiraLivre`, given the validated data.
-    #     """
-    #     instance.title = validated_data.get('title', instance.title)
-    #     instance.code = validated_data.get('code', instance.code)
-    #     instance.linenos = validated_data.get('linenos', instance.linenos)
-    #     instance.language = validated_data.get('language', instance.language)
-    #     instance.style = validated_data.get('style', instance.style)
-    #     instance.save()
-    #     return instance
+class Feiras(models.Model):
+    # cod = models.IntegerField(read_only=True)
+    # By default, Django gives each model the
+    # id = models.AutoField(primary_key=True)
+    longi = models.CharField(requerid=True, allow_blank=True, max_length=20)
+    lat = models.CharField(requerid=True, allow_blank=True, max_length=20)
+    setcens = models.IntegerField(requerid=True, allow_blank=True,
+                                  max_length=20)
+    areap = models.IntegerField(requerid=True, allow_blank=True, max_length=20)
+    coddist = models.IntegerField(requerid=True, allow_blank=True,
+                                  max_length=5)
+    distrito = models.CharField(requerid=True, allow_blank=True, max_length=20)
+    codsubpref = models.IntegerField(requerid=True, allow_blank=True,
+                                     max_length=5)
+    subprefe = models.CharField(requerid=True, allow_blank=True, max_length=50)
+    regiao5 = models.CharField(requerid=True, allow_blank=True, max_length=20)
+    regiao8 = models.CharField(requerid=True, allow_blank=True, max_length=20)
+    nome_feira = models.CharField(requerid=True, allow_blank=True,
+                                  max_length=50)
+    registro = models.CharField(requerid=True, allow_blank=True, max_length=20)
+    logradouro = models.CharField(requerid=True, allow_blank=True,
+                                  max_length=50)
+    numero = models.IntegerField(requerid=True, allow_blank=True, max_length=6)
+    bairro = models.CharField(requerid=True, allow_blank=True, max_length=50)
+    referencia = models.CharField(requerid=True, allow_blank=True,
+                                  max_length=50)
